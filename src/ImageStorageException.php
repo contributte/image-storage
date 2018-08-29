1658ba68d64180f784132331e2d67d808b396f7f
tremchez <tremchez@gmail.com>
2018-08-29 17:23:12 +0200

    Handle failed creation of no_image.png

---
+++ src/ImageStorageException.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * @copyright   Copyright (c) 2018 ublaboo <ublaboo@paveljanda.com>
+ * @author      Vojta Tremcinsky <me@paveljanda.com>
+ * @package     Ublaboo
+ */
+
+namespace Ublaboo\ImageStorage;
+
+
+class ImageStorageException extends \Exception
+{
+}
