0331a294934e705a7d4099b90a4b139f4c5bae2c
Adam Žurek <thejabko@gmail.com>
2019-03-12 11:49:16 +0100

    tests/bootstrap.php: adde black line after if

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,5 +6,6 @@ if (@! include __DIR__ . '/../vendor/autoload.php') {
 	echo 'Install Nette Tester using `composer update --dev`';
 	exit(1);
 }
+
 // Configure environment
 Environment::setup(__DIR__);
