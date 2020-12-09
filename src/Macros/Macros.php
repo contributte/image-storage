55fb9026373a1120c95e07d8a00363497d5d20bf
Michal Hlávka <michal.hlavka@gmail.com>
2020-12-09 22:39:49 +0100

    Fixed Macros

--- src/Macros/Macros.php
+++ src/Macros/Macros.php
@@ -26,37 +26,37 @@ final class Macros extends Latte\Macros\MacroSet
 
 	public function tagImg(MacroNode $node, PhpWriter $writer): string
 	{
-		return $writer->write('$_img = $imageStorage->fromIdentifier(%node.array); echo "<img src=\"" . $basePath . "/" . $_img->createLink() . "\">";');
+		return $writer->write('$_img = $_imageStorage->fromIdentifier(%node.array); echo "<img src=\"" . $basePath . "/" . $_img->createLink() . "\">";');
 	}
 
 
 	public function tagImgAbs(MacroNode $node, PhpWriter $writer): string
 	{
-		return $writer->write('$_img = $imageStorage->fromIdentifier(%node.array); echo "<img src=\"" . $baseUrl . "/" . $_img->createLink() . "\">";');
+		return $writer->write('$_img = $_imageStorage->fromIdentifier(%node.array); echo "<img src=\"" . $baseUrl . "/" . $_img->createLink() . "\">";');
 	}
 
 
 	public function attrImg(MacroNode $node, PhpWriter $writer): string
 	{
-		return $writer->write('$_img = $imageStorage->fromIdentifier(%node.array); echo \' src="\' . $basePath . "/" . $_img->createLink() . \'"\'');
+		return $writer->write('$_img = $_imageStorage->fromIdentifier(%node.array); echo \' src="\' . $basePath . "/" . $_img->createLink() . \'"\'');
 	}
 
 
 	public function attrImgAbs(MacroNode $node, PhpWriter $writer): string
 	{
-		return $writer->write('$_img = $imageStorage->fromIdentifier(%node.array); echo \' src="\' . $baseUrl . "/" . $_img->createLink() . \'"\'');
+		return $writer->write('$_img = $_imageStorage->fromIdentifier(%node.array); echo \' src="\' . $baseUrl . "/" . $_img->createLink() . \'"\'');
 	}
 
 
 	public function linkImg(MacroNode $node, PhpWriter $writer): string
 	{
-		return $writer->write('$_img = $imageStorage->fromIdentifier(%node.array); echo $basePath . "/" . $_img->createLink()');
+		return $writer->write('$_img = $_imageStorage->fromIdentifier(%node.array); echo $basePath . "/" . $_img->createLink()');
 	}
 
 
 	public function linkImgAbs(MacroNode $node, PhpWriter $writer): string
 	{
-		return $writer->write('$_img = $imageStorage->fromIdentifier(%node.array); echo $baseUrl . "/" . $_img->createLink()');
+		return $writer->write('$_img = $_imageStorage->fromIdentifier(%node.array); echo $baseUrl . "/" . $_img->createLink()');
 	}
 
 }
