5d0ed66f5f3ae7b4774a2fe06ddb7540dfd82b78
Pavel W <pavel.weirich@makeart.cz>
2022-04-05 17:10:05 +0200

    Tests correction: - prefix must contain 2 characters - add new test for deleting only changed images

--- tests/cases/ImageNameScriptTest.php
+++ tests/cases/ImageNameScriptTest.php
@@ -13,32 +13,46 @@ final class ImageNameScriptTest extends BaseTestCase
 
 	public function testFromName(): void
 	{
-		$s = ImageNameScript::fromName('/data/images/ed/kitty.100x200.fill.q100.jpg');
+		$s = ImageNameScript::fromName('images/49/kitty.100x200.fill.q100.jpg');
 
-		Assert::same($s->original, '/data/images/ed/kitty.jpg');
-		Assert::same($s->prefix, 'data');
-		Assert::same($s->name, 'images/ed/kitty');
+		Assert::same($s->original, 'images/49/kitty.jpg');
+		Assert::same($s->namespace, 'images');
+		Assert::same($s->prefix, '49');
+		Assert::same($s->name, 'kitty');
 		Assert::same($s->flag, 'fill');
 		Assert::same($s->quality, '100');
 		Assert::same($s->size, [100, 200]);
 		Assert::same($s->extension, 'jpg');
 		Assert::same($s->crop, []);
 
-		$s = ImageNameScript::fromName('/data/images/ed/kitty.200x200crop100x150x100x100.fit.q85.jpg');
+		$s = ImageNameScript::fromName('images/10/20/49/kitty.100x200.fill.q100.jpg');
+
+		Assert::same($s->original, 'images/10/20/49/kitty.jpg');
+		Assert::same($s->namespace, 'images/10/20');
+		Assert::same($s->prefix, '49');
+		Assert::same($s->name, 'kitty');
+		Assert::same($s->flag, 'fill');
+		Assert::same($s->quality, '100');
+		Assert::same($s->size, [100, 200]);
+		Assert::same($s->extension, 'jpg');
+		Assert::same($s->crop, []);
+
+
+		$s = ImageNameScript::fromName('/data/images/49/kitty.200x200crop100x150x100x100.fit.q85.jpg');
 		Assert::same($s->crop, [100, 150, 100, 100]);
 	}
 
 
 	public function testFromIdentifier(): void
 	{
-		$s = ImageNameScript::fromIdentifier('images/ed/kitty.jpg');
+		$s = ImageNameScript::fromIdentifier('images/49/kitty.jpg');
 
 		$s->setQuality(2);
 		$s->setSize([2, 2]);
 		$s->setFlag('exact');
 
-		Assert::same($s->getIdentifier(), 'images/ed/kitty.2x2.exact.q2.jpg');
-		Assert::same($s->toQuery(), 'images/ed/2x2.exact.q2/kitty.jpg?_image_storage');
+		Assert::same($s->getIdentifier(), 'images/49/kitty.2x2.exact.q2.jpg');
+		Assert::same($s->toQuery(), 'images/49/2x2.exact.q2/kitty.jpg?_image_storage');
 	}
 
 }
